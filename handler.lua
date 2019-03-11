d1935332844aea14f9321c52e1df4f80726d2c27
Иван <vanyarock01@gmail.com>
2019-03-11 15:10:03 +0300

    Добавлен метод POST

--- handler.lua
+++ handler.lua
@@ -2,10 +2,26 @@ local log = require('log')
 local json = require('json')
 
 local handler = {}
-    
+
 handler.post_method = function(self)
-    log.info("POST_METHOD")
-    return {}
+    local body = self:json()
+    local key, val = body['key'], body['value']
+
+    if type(key) ~= 'string' or
+            type(val) ~= 'table' then
+        return {status = 400} 
+    end
+    status, ret = pcall(box.space.dict.insert, box.space.dict, {key, value})
+    log.info(ret)
+    if not status then
+        if ret.code == 3 then -- ER_TUPLE_FOUND 
+            return {status = 409}
+        else
+            return {status = 500}
+        end
+    end 
+
+    return {status = 200}
 end
 
 handler.put_method = function(self)
